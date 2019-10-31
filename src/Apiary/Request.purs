b39f51914d170c741053c4cac3307fe4c863cb91
Robert Porter <robert.porter@rakuten.com>
2019-10-31 16:20:34 +0900

    First version of a Requestable instance

--- src/Apiary/Request.purs
+++ src/Apiary/Request.purs
@@ -1,27 +1,51 @@
 module Apiary.Request where
 
 import Prelude
-import Apiary.Params (class WriteParams)
-import Apiary.Response (class DecodeResponse)
-import Apiary.Route (Route)
-import Data.Symbol (class IsSymbol)
-import Effect.Aff (Aff)
-import Milkis (Headers)
-import Type.Equality (class TypeEquals)
-import Unsafe.Coerce (unsafeCoerce)
+
+import Apiary.Response (class DecodeResponse, decodeResponse)
+import Apiary.Types (Request)
+import Control.Comonad (extract)
+import Control.Monad.Except (runExcept, throwError)
+import Data.Either (Either(..))
+import Data.Symbol (SProxy(..))
+import Effect.Aff (Aff, error)
+import Effect.Class.Console as Console
+import Foreign (renderForeignError)
+import Milkis (fetch, headers, statusCode) as Milkis
+import Milkis as Milkil
+import Milkis.Impl.Window (windowFetch) as Milkis
+import Record as Record
+import Type.Proxy (Proxy(..))
 
 class Requestable route params body response | route -> params body response where
-  makeRequest :: route -> (Headers -> Headers) -> params -> body -> Aff response
+  makeRequest :: route -> (Request -> Request) -> params -> body -> Aff response
 
-instance requestableRoute ::
-  ( IsSymbol path
-  , TypeEquals
-      spec
-      { params :: params
-      , response :: response'
-      }
-  , WriteParams params
-  , DecodeResponse response' response
+class BuildRequest route params body rep | route -> params body rep where
+  buildRequest :: route -> params -> body -> Request
+
+instance requestable ::
+  ( BuildRequest route params body rep
+  , DecodeResponse rep response
   ) =>
-  Requestable (Route "GET" path spec) params body response where
-  makeRequest = unsafeCoerce unit
+  Requestable route params body response where
+  makeRequest route transform params body = decode =<< fetch request
+    where
+    request = transform $ buildRequest route params body
+
+    fetch req = do
+      response <- Milkis.fetch Milkis.windowFetch request.url $ Record.delete (SProxy :: _ "url") req
+      text <- Milkil.text response
+      let
+        status = Milkis.statusCode response
+
+        headers = Milkis.headers response
+      pure { status, headers, body: text }
+
+    decode =
+      decodeResponse (Proxy :: _ rep)
+        >>> runExcept
+        >>> case _ of
+            Left errors -> do
+              Console.logShow $ renderForeignError <$> errors
+              throwError $ error $ renderForeignError $ extract errors
+            Right response -> pure response
