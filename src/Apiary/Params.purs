2479cde0b3d2c89f383598db6b56913cd74aed22
Robert Porter <robert.porter@rakuten.com>
2019-11-05 17:24:47 +0900

    Big rework of Params, which allowed implementation of Route

--- src/Apiary/Params.purs
+++ src/Apiary/Params.purs
@@ -1,14 +1,17 @@
 module Apiary.Params where
 
 import Prelude
+import Data.Foldable (intercalate)
+import Data.Maybe (Maybe(..))
 import Data.String (Pattern(..), Replacement(..))
 import Data.String as String
 import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
 import Global.Unsafe (unsafeEncodeURIComponent)
-import Prim.Row (class Cons)
-import Prim.RowList (class RowToList, Cons, Nil)
+import Prim.Row (class Cons, class Nub, class Union)
+import Prim.RowList (kind RowList, class RowToList, Cons, Nil)
 import Record as Record
 import Type.Data.RowList (RLProxy(..))
+import Type.Proxy (Proxy)
 import Unsafe.Coerce (unsafeCoerce)
 
 class EncodeParam param where
@@ -20,36 +23,112 @@ instance encodeParamInt :: EncodeParam Int where
 instance encodeParamString :: EncodeParam String where
   encodeParam = unsafeEncodeURIComponent
 
-class WriteParams params where
-  writeParams :: params -> String -> String
-
-instance writeParamsUnit :: WriteParams Unit where
-  writeParams _ = identity
+class WriteParams pathParams queryParams params | pathParams queryParams -> params where
+  writeParams :: Proxy pathParams -> Proxy queryParams -> params -> String -> String
 
 instance writeParamsRecord ::
-  ( RowToList params paramList
-  , WriteParamRecord params paramList
+  ( Union pathParams queryParamRep mergedParams
+  , Nub mergedParams cleanParams
+  , RowToList pathParams pathParamList
+  , RowToList queryParams queryParamList
+  , WritePathParams pathParams pathParamList
+  , BuildQueryParams queryParamRep queryParamList
   ) =>
-  WriteParams { | params } where
-  writeParams = writeParamRecord (RLProxy :: _ paramList)
-
-class WriteParamRecord params paramList | paramList -> params where
-  writeParamRecord :: RLProxy paramList -> Record params -> String -> String
-
-instance writeParamRecordNil :: WriteParamRecord () Nil where
-  writeParamRecord _ _ = identity
-
-instance writeParamRecordCons ::
-  ( Cons name param params' params
-  , IsSymbol name
-  , EncodeParam param
-  , WriteParamRecord params' paramList
-  ) =>
-  WriteParamRecord params (Cons name param paramList) where
-  writeParamRecord _ params url = writeParamRecord (RLProxy :: _ paramList) (unsafeCoerce params) modifiedUrl
+  WriteParams (Record pathParams) (Record queryParams) (Record cleanParams) where
+  writeParams _ _ params url = path <> prefix query
     where
-    modifiedUrl = String.replace (Pattern pattern) (Replacement replacement) url
+    coercePathParams = unsafeCoerce :: Record cleanParams -> Record pathParams
 
-    pattern = "<" <> reflectSymbol (SProxy :: _ name) <> ">"
+    coerceQueryParams = unsafeCoerce :: Record cleanParams -> Record queryParamRep
+
+    path = writePathParams (RLProxy :: _ pathParamList) (coercePathParams params) url
+
+    query = intercalate "&" $ buildQueryParams (RLProxy :: _ queryParamList) (coerceQueryParams params)
+
+    prefix q = if String.null q then "" else "?" <> q
+
+class WritePathParams (params :: #Type) (paramList :: RowList) | paramList -> params where
+  writePathParams :: RLProxy paramList -> Record params -> String -> String
+
+instance writePathParamsNil :: WritePathParams () Nil where
+  writePathParams _ _ = identity
+
+instance writePathParamsCons ::
+  ( IsSymbol name
+  , EncodeParam value
+  , Cons name value params' params
+  , WritePathParams params' paramTail
+  ) =>
+  WritePathParams params (Cons name value paramTail) where
+  writePathParams _ params url =
+    writePathParams (RLProxy :: _ paramTail) (coerceParams params)
+      $ String.replace (Pattern pattern) (Replacement replacement) url
+    where
+    coerceParams = unsafeCoerce :: Record params -> Record params'
+
+    pattern = ":" <> reflectSymbol (SProxy :: _ name)
 
     replacement = encodeParam $ Record.get (SProxy :: _ name) params
+
+class BuildQueryParams (params :: #Type) (paramList :: RowList) | paramList -> params where
+  buildQueryParams :: RLProxy paramList -> Record params -> Array String
+
+instance buildQueryParamsNil :: BuildQueryParams () Nil where
+  buildQueryParams _ _ = []
+
+instance buildQueryParamsConsArray ::
+  ( IsSymbol name
+  , EncodeParam value
+  , Cons name (Maybe (Array value)) params' params
+  , BuildQueryParams params' paramTail
+  ) =>
+  BuildQueryParams params (Cons name (Array value) paramTail) where
+  buildQueryParams _ params = query <> buildQueryParams (RLProxy :: _ paramTail) (coerceParams params)
+    where
+    coerceParams = unsafeCoerce :: Record params -> Record params'
+
+    name' = SProxy :: _ name
+
+    name = reflectSymbol name'
+
+    query =
+      case Record.get name' params of
+        Nothing -> []
+        Just values -> values <#> encodeQueryParam name
+else instance buildQueryParamsCons ::
+  ( IsSymbol name
+  , EncodeParam value
+  , Cons name (Maybe value) params' params
+  , BuildQueryParams params' paramTail
+  ) =>
+  BuildQueryParams params (Cons name value paramTail) where
+  buildQueryParams _ params = query <> buildQueryParams (RLProxy :: _ paramTail) (coerceParams params)
+    where
+    coerceParams = unsafeCoerce :: Record params -> Record params'
+
+    name' = SProxy :: _ name
+
+    name = reflectSymbol name'
+
+    query =
+      case Record.get name' params of
+        Nothing -> []
+        Just value -> [ encodeQueryParam name value ]
+
+encodeQueryParam :: forall name value. EncodeParam name => EncodeParam value => name -> value -> String
+encodeQueryParam name value = encodeParam name <> "=" <> encodeParam value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Apiary/Route.purs
+++ src/Apiary/Route.purs
@@ -1,7 +1,47 @@
 module Apiary.Route where
 
+import Apiary.Types (Request)
+import Apiary.Body (class EncodeBody, encodeBody)
+import Apiary.Params (class WriteParams, writeParams)
+import Apiary.Request (class BuildRequest)
+import Apiary.Response (class DecodeResponse)
+import Data.Symbol (class IsSymbol, reflectSymbol)
+import Data.Unit (Unit)
+import Data.Variant (SProxy(..))
+import Foreign.Object as Object
+import Prim.Row (class Nub, class Union)
+import Type.Equality (class TypeEquals)
+import Type.Proxy (Proxy(..))
+import Unsafe.Coerce (unsafeCoerce)
+
 data Route (method :: Symbol) (path :: Symbol) spec
   = Route
+
+type SpecDefaults
+  = ( params :: {}
+    , query :: {}
+    , body :: Unit
+    , response :: Unit
+    )
+
+class PrepareSpec spec prepared | spec -> prepared
+
+instance prepareSpec ::
+  ( Union spec SpecDefaults specWithDefaults
+  , Nub specWithDefaults prepared
+  , TypeEquals
+      (Record prepared)
+      { params :: params
+      , query :: query
+      , body :: body
+      , response :: response
+      }
+  -- , WriteParams params query
+  -- , WriteQuery query
+  , EncodeBody body body'
+  , DecodeResponse response response'
+  ) =>
+  PrepareSpec (Record spec) (Record prepared)
 
 type GET
   = Route "GET"
@@ -17,3 +57,128 @@ type PUT
 
 type DELETE
   = Route "DELETE"
+
+instance buildRequestRouteGET ::
+  ( PrepareSpec
+      spec
+      { params :: params
+      , query :: query
+      , body :: Unit
+      , response :: response
+      }
+  , WriteParams params query fullParams
+  , DecodeResponse response response'
+  , IsSymbol path
+  ) =>
+  BuildRequest (Route "GET" path spec) fullParams Unit response where
+  buildRequest _ =
+    buildRequest_
+      "GET"
+      (SProxy :: _ path)
+      (Proxy :: _ params)
+      (Proxy :: _ query)
+      (Proxy :: _ Unit)
+else instance buildRequestRoutePATCH ::
+  ( PrepareSpec
+      spec
+      { params :: params
+      , query :: query
+      , body :: body
+      , response :: response
+      }
+  , WriteParams params query fullParams
+  , EncodeBody body body'
+  , DecodeResponse response response'
+  , IsSymbol path
+  ) =>
+  BuildRequest (Route "PATCH" path spec) fullParams body' response where
+    buildRequest _ =
+      buildRequest_
+        "PATCH"
+        (SProxy :: _ path)
+        (Proxy :: _ params)
+        (Proxy :: _ query)
+        (Proxy :: _ body)
+else instance buildRequestRoutePOST ::
+  ( PrepareSpec
+      spec
+      { params :: params
+      , query :: query
+      , body :: body
+      , response :: response
+      }
+  , WriteParams params query fullParams
+  , EncodeBody body body'
+  , DecodeResponse response response'
+  , IsSymbol path
+  ) =>
+  BuildRequest (Route "POST" path spec) fullParams body' response where
+    buildRequest _ =
+      buildRequest_
+        "POST"
+        (SProxy :: _ path)
+        (Proxy :: _ params)
+        (Proxy :: _ query)
+        (Proxy :: _ body)
+else instance buildRequestRoutePUT ::
+  ( PrepareSpec
+      spec
+      { params :: params
+      , query :: query
+      , body :: body
+      , response :: response
+      }
+  , WriteParams params query fullParams
+  , EncodeBody body body'
+  , DecodeResponse response response'
+  , IsSymbol path
+  ) =>
+  BuildRequest (Route "PUT" path spec) fullParams body' response where
+    buildRequest _ =
+      buildRequest_
+        "PUT"
+        (SProxy :: _ path)
+        (Proxy :: _ params)
+        (Proxy :: _ query)
+        (Proxy :: _ body)
+else instance buildRequestRouteDELETE ::
+  ( PrepareSpec
+      spec
+      { params :: params
+      , query :: query
+      , body :: body
+      , response :: response
+      }
+  , WriteParams params query fullParams
+  , EncodeBody body body'
+  , DecodeResponse response response'
+  , IsSymbol path
+  ) =>
+  BuildRequest (Route "DELETE" path spec) fullParams body' response where
+    buildRequest _ =
+      buildRequest_
+        "DELETE"
+        (SProxy :: _ path)
+        (Proxy :: _ params)
+        (Proxy :: _ query)
+        (Proxy :: _ body)
+
+buildRequest_ ::
+  forall path pathParams queryParams params bodyRep body.
+  IsSymbol path =>
+  WriteParams pathParams queryParams params =>
+  EncodeBody bodyRep body =>
+  String ->
+  SProxy path ->
+  Proxy pathParams ->
+  Proxy queryParams ->
+  Proxy bodyRep ->
+  params ->
+  body ->
+  Request
+buildRequest_ method path pathParams queryParams bodyRep params body =
+  { method: unsafeCoerce method
+  , url: unsafeCoerce (writeParams pathParams queryParams params (reflectSymbol path))
+  , headers: Object.empty
+  , body: encodeBody bodyRep body
+  }
