756f3aad63fe15073e52ac6941e94cf84335dc12
Robert Porter <robert.porter@rakuten.com>
2019-11-12 13:35:44 +0900

    Inlined `removeEmptyBody` logic into `fetch`

--- src/Apiary.purs
+++ src/Apiary.purs
@@ -8,13 +8,10 @@ import Control.Comonad (extract)
 import Control.Monad.Error.Class (try)
 import Control.Monad.Except (ExceptT(..), mapExceptT, runExceptT, withExceptT)
 import Data.Either (Either)
-import Data.String as String
 import Effect.Aff (Aff)
 import Milkis (fetch, headers, statusCode, text) as Milkis
 import Milkis.Impl.Window (windowFetch) as Milkis
-import Simple.JSON (undefined)
 import Type.Proxy (Proxy(..))
-import Unsafe.Coerce (unsafeCoerce)
 
 makeRequest ::
   forall route params body rep response.
@@ -28,10 +25,7 @@ makeRequest ::
 makeRequest route transform params body = runExceptT $ decode =<< fetch request
   where
   request :: Request
-  request =
-    buildRequest route params body
-      # transform
-      # removeEmptyBody
+  request = transform $ buildRequest route params body
 
   decode :: Response -> ExceptT Error Aff response
   decode text = mapExceptT (pure <<< extract) $ decodeResponse (Proxy :: _ rep) text
@@ -40,18 +34,13 @@ lift :: Aff ~> ExceptT Error Aff
 lift = withExceptT RuntimeError <<< ExceptT <<< try
 
 fetch :: Request -> ExceptT Error Aff Response
-fetch { method, url, headers, body } = do
-  response <- lift $ Milkis.fetch Milkis.windowFetch url { method, headers, body }
+fetch request@{ method, url, headers } = do
+  response <- case request.body of
+    "" -> lift $ Milkis.fetch Milkis.windowFetch url { method, headers }
+    body -> lift $ Milkis.fetch Milkis.windowFetch url { method, headers, body }
   text <- lift $ Milkis.text response
   pure
     { status: Milkis.statusCode response
     , headers: Milkis.headers response
     , body: text
     }
-
-removeEmptyBody :: Request -> Request
-removeEmptyBody request@{ body } =
-  if String.null body then
-    request { body = unsafeCoerce undefined }
-  else
-    request
