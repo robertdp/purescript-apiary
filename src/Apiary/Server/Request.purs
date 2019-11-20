9ceebd95ef24cc60a3c0ae7ac9d577378b10d2df
Robert Porter <robert.porter@rakuten.com>
2019-11-20 15:30:24 +0900

    Added readBodyAsBuffer and readBodyAsString

--- src/Apiary/Server/Request.purs
+++ src/Apiary/Server/Request.purs
@@ -4,9 +4,19 @@ import Prelude
 import Apiary.Media (class MediaCodec, decodeMedia)
 import Apiary.Route (class PrepareSpec, Route)
 import Apiary.Server.Url (class ReadParams, PathParams, QueryParams, readParams)
+import Data.Either (Either(..), either)
+import Effect.Aff (Aff, launchAff_, throwError)
+import Effect.Aff.AVar as AVar
+import Effect.Class (liftEffect)
+import Effect.Exception (catchException)
 import Foreign (F)
 import Foreign.Object (Object)
 import Foreign.Object as Object
+import Node.Buffer (Buffer)
+import Node.Buffer as Buffer
+import Node.Encoding as Encoding
+import Node.HTTP as HTTP
+import Node.Stream as Stream
 import Type.Proxy (Proxy(..))
 
 type Request params body
@@ -34,3 +44,36 @@ instance decodeRequestRoute ::
     params <- readParams (Proxy :: _ params) (Proxy :: _ query) pathParams queryParams
     body <- decodeMedia (Proxy :: _ body) requestBody
     pure { params, headers: Object.empty, body }
+
+readBodyAsBuffer :: HTTP.Request -> Aff Buffer
+readBodyAsBuffer request = do
+  let
+    stream = HTTP.requestAsStream request
+  bodyResult <- AVar.empty
+  chunks <- AVar.new []
+  fillResult <-
+    liftEffect
+      $ catchException (pure <<< Left) (Right <$> fillBody stream chunks bodyResult)
+  body <- AVar.take bodyResult
+  either throwError pure (fillResult *> body)
+  where
+  fillBody stream chunks bodyResult = do
+    Stream.onData stream \chunk ->
+      let
+        modification = do
+          v <- AVar.take chunks
+          AVar.put (v <> [ chunk ]) chunks
+      in
+        launchAff_ modification
+    Stream.onError stream (launchAff_ <<< flip AVar.put bodyResult <<< Left)
+    Stream.onEnd stream do
+      launchAff_ do
+        AVar.take chunks
+          >>= concat'
+          >>= (pure <<< Right)
+          >>= flip AVar.put bodyResult
+
+  concat' = liftEffect <<< Buffer.concat
+
+readBodyAsString :: HTTP.Request -> Aff String
+readBodyAsString = readBodyAsBuffer >=> liftEffect <<< Buffer.toString Encoding.UTF8
