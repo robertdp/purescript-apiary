50734f0a94b7094461ae550d5e68080f85649d77
Robert Porter <robert.porter@rakuten.com>
2019-12-23 12:52:12 +0900

    Merged the client code back into the main project

---
+++ src/Apiary/Client/Response.purs
@@ -0,0 +1,62 @@
+module Apiary.Client.Response where
+
+import Prelude
+import Apiary.Media (class MediaCodec, decodeMedia)
+import Apiary.Status (class ResponseStatus)
+import Apiary.Status as Status
+import Apiary.Types (Apiary, Error(..), Response)
+import Control.Alt ((<|>))
+import Control.Monad.Error.Class (throwError)
+import Control.Monad.Except (withExcept)
+import Data.Symbol (class IsSymbol)
+import Data.Variant (SProxy(..), Variant, expand, inj)
+import Prim.Row (class Cons, class Union)
+import Prim.RowList (class RowToList, kind RowList, Cons, Nil)
+import Type.Data.RowList (RLProxy(..))
+import Type.Proxy (Proxy(..))
+
+class DecodeResponse rep response | rep -> response where
+  decodeResponse :: Proxy rep -> Response -> Apiary response
+
+instance decodeResponseUnit :: DecodeResponse Unit Unit where
+  decodeResponse _ _ = pure unit
+
+instance decodeResponseString :: DecodeResponse String String where
+  decodeResponse _ response = pure response.body
+
+instance decodeResponseRecord ::
+  ( RowToList responses responseList
+  , DecodeResponseVariant result responseList
+  ) =>
+  DecodeResponse { | responses } (Variant result) where
+  decodeResponse _ = decodeResponseVariant (RLProxy :: _ responseList)
+
+class DecodeResponseVariant (response :: #Type) (responseList :: RowList) | responseList -> response where
+  decodeResponseVariant :: RLProxy responseList -> Response -> Apiary (Variant response)
+
+instance decodeResponseVariantNil :: DecodeResponseVariant () Nil where
+  -- | This will never be reached if the data originates from PureScript.
+  decodeResponseVariant _ = throwError <<< UnexpectedResponse
+
+instance decodeResponseVariantCons ::
+  ( IsSymbol status
+  , ResponseStatus status
+  , Cons status decoded variant' variant
+  , DecodeResponseVariant variant' responseList
+  , Union variant' a variant
+  , MediaCodec rep decoded
+  ) =>
+  DecodeResponseVariant variant (Cons status rep responseList) where
+  decodeResponseVariant _ response =
+    (inj status <$> decodeStatus)
+      <|> (expand <$> decodeRest)
+    where
+    status = SProxy :: _ status
+
+    statusCode = Status.statusCode (Status.toStatus status)
+
+    decodeStatus
+      | response.status == statusCode = withExcept (flip DecodeError response) $ decodeMedia (Proxy :: _ rep) response.body
+      | otherwise = throwError $ UnexpectedResponse response
+
+    decodeRest = decodeResponseVariant (RLProxy :: _ responseList) response
---
+++ src/Apiary/Client/Url.purs
@@ -0,0 +1,107 @@
+module Apiary.Client.Url where
+
+import Prelude
+import Apiary.Url as Url
+import Data.Either (either)
+import Data.Foldable (intercalate)
+import Data.Maybe (Maybe, maybe)
+import Data.String as String
+import Data.String.Regex as Regex
+import Data.String.Regex.Flags as RegexFlags
+import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
+import Data.Tuple (Tuple(..), fst, snd)
+import Prim.Row (class Cons, class Nub, class Union)
+import Prim.RowList (kind RowList, class RowToList, Cons, Nil)
+import Record as Record
+import Type.Data.RowList (RLProxy(..))
+import Type.Proxy (Proxy)
+import Unsafe.Coerce (unsafeCoerce)
+
+class WriteParams pathParams queryParams params | pathParams queryParams -> params where
+  writeParams :: Proxy pathParams -> Proxy queryParams -> params -> String -> String
+
+instance writeParamsRecord ::
+  ( Union pathParams queryParamRep dirtyParams
+  , Nub dirtyParams mergedParams
+  , RowToList pathParams pathParamList
+  , RowToList queryParams queryParamList
+  , WritePathParams pathParams pathParamList
+  , BuildQueryParams queryParamRep queryParamList
+  ) =>
+  WriteParams (Record pathParams) (Record queryParams) (Record mergedParams) where
+  writeParams _ _ params url = path <> prefix query
+    where
+    coercePathParams = unsafeCoerce :: Record mergedParams -> Record pathParams
+
+    coerceQueryParams = unsafeCoerce :: Record mergedParams -> Record queryParamRep
+
+    path = writePathParams (RLProxy :: _ pathParamList) (coercePathParams params) url
+
+    query =
+      buildQueryParams (RLProxy :: _ queryParamList) (coerceQueryParams params)
+        # map (\param -> fst param <> "=" <> snd param)
+        # intercalate "&"
+
+    prefix q = if String.null q then "" else "?" <> q
+
+class WritePathParams (params :: #Type) (paramList :: RowList) | paramList -> params where
+  writePathParams :: RLProxy paramList -> Record params -> String -> String
+
+instance writePathParamsNil :: WritePathParams () Nil where
+  writePathParams _ _ = identity
+
+instance writePathParamsCons ::
+  ( IsSymbol name
+  , Url.EncodeParam value
+  , Cons name value params' params
+  , WritePathParams params' paramTail
+  ) =>
+  WritePathParams params (Cons name value paramTail) where
+  writePathParams _ params url = writePathParams (RLProxy :: _ paramTail) (coerceParams params) replaced
+    where
+    coerceParams = unsafeCoerce :: Record params -> Record params'
+
+    regex = Regex.regex ("\\:" <> reflectSymbol (SProxy :: _ name) <> "\\b") RegexFlags.global
+
+    replaced = either (const url) (\pattern -> Regex.replace pattern replacement url) regex
+
+    replacement = Url.encodeParam $ Record.get (SProxy :: _ name) params
+
+class BuildQueryParams (params :: #Type) (paramList :: RowList) | paramList -> params where
+  buildQueryParams :: RLProxy paramList -> Record params -> Array (Tuple String String)
+
+instance buildQueryParamsNil :: BuildQueryParams () Nil where
+  buildQueryParams _ _ = []
+
+instance buildQueryParamsConsArray ::
+  ( IsSymbol name
+  , Url.EncodeParam value
+  , Cons name (Array value) params' params
+  , BuildQueryParams params' paramTail
+  ) =>
+  BuildQueryParams params (Cons name (Array value) paramTail) where
+  buildQueryParams _ params = query <> buildQueryParams (RLProxy :: _ paramTail) (coerceParams params)
+    where
+    coerceParams = unsafeCoerce :: Record params -> Record params'
+
+    name' = SProxy :: _ name
+
+    name = Url.encodeParam $ reflectSymbol name'
+
+    query = (Tuple name <<< Url.encodeParam) <$> Record.get name' params
+else instance buildQueryParamsCons ::
+  ( IsSymbol name
+  , Url.EncodeParam value
+  , Cons name (Maybe value) params' params
+  , BuildQueryParams params' paramTail
+  ) =>
+  BuildQueryParams params (Cons name value paramTail) where
+  buildQueryParams _ params = query <> buildQueryParams (RLProxy :: _ paramTail) (coerceParams params)
+    where
+    coerceParams = unsafeCoerce :: Record params -> Record params'
+
+    name' = SProxy :: _ name
+
+    name = Url.encodeParam $ reflectSymbol name'
+
+    query = maybe [] (pure <<< Tuple name <<< Url.encodeParam) $ Record.get name' params
