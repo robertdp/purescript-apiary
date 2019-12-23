fef6dd658e90f8efb80ada4be056f6fd65bf6ab0
Robert Porter <robert.porter@rakuten.com>
2019-12-23 12:30:12 +0900

    Splitting out the server and client parts into separate libraries

--- packages.dhall
+++ packages.dhall
@@ -119,7 +119,7 @@ let additions =
 
 
 let upstream =
-      https://github.com/purescript/package-sets/releases/download/psc-0.13.4-20191110/packages.dhall sha256:563a7f694e18e6399f7f6d01f5b7e3c3345781655d99945768f48e458feb93a4
+      https://github.com/purescript/package-sets/releases/download/psc-0.13.5-20191215/packages.dhall sha256:fdc5d54cd54213000100fbc13c90dce01668a73fe528d8662430558df3411bee
 
 let overrides = {=}
 
--- src/Apiary/Url.purs
+++ src/Apiary/Url.purs
@@ -5,18 +5,32 @@ import Foreign (F)
 import Global.Unsafe (unsafeDecodeURIComponent, unsafeEncodeURIComponent)
 import Simple.JSON (readJSON', writeJSON)
 
-class UrlParam a where
-  encodeUrlParam :: a -> String
-  decodeUrlParam :: String -> F a
+class EncodeParam a where
+  encodeParam :: a -> String
 
-instance urlParamString :: UrlParam String where
-  encodeUrlParam = unsafeEncodeURIComponent
-  decodeUrlParam = unsafeDecodeURIComponent >>> pure
+class DecodeParam a where
+  decodeParam :: String -> F a
 
-instance urlParamInt :: UrlParam Int where
-  encodeUrlParam = encodeUrlParam <<< writeJSON
-  decodeUrlParam = decodeUrlParam >=> readJSON'
+instance encodeParamString :: EncodeParam String where
+  encodeParam = unsafeEncodeURIComponent
 
-instance urlParamNumber :: UrlParam Number where
-  encodeUrlParam = encodeUrlParam <<< writeJSON
-  decodeUrlParam = decodeUrlParam >=> readJSON'
+instance decodeParamString :: DecodeParam String where
+  decodeParam = pure <<< unsafeDecodeURIComponent
+
+instance encodeParamBoolean :: EncodeParam Boolean where
+  encodeParam = encodeParam <<< writeJSON
+
+instance decodeParamBoolean :: DecodeParam Boolean where
+  decodeParam = decodeParam >=> readJSON'
+
+instance encodeParamInt :: EncodeParam Int where
+  encodeParam = encodeParam <<< writeJSON
+
+instance decodeParamInt :: DecodeParam Int where
+  decodeParam = decodeParam >=> readJSON'
+
+instance encodeParamNumber :: EncodeParam Number where
+  encodeParam = encodeParam <<< writeJSON
+
+instance decodeParamNumber :: DecodeParam Number where
+  decodeParam = decodeParam >=> readJSON'
