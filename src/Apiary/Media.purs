93b0ce57daf250b894b0f390e8eaa06902351525
Robert Porter <robert.porter@rakuten.com>
2020-02-10 14:35:10 +0900

    Added custom JSON encoding for Variant

--- src/Apiary/Media.purs
+++ src/Apiary/Media.purs
@@ -11,11 +11,22 @@ module Apiary.Media
   ) where
 
 import Prelude
+import Control.Alt ((<|>))
 import Data.Maybe (Maybe(..))
 import Data.MediaType (MediaType)
 import Data.MediaType.Common (applicationJSON, textPlain)
-import Foreign (F)
-import Simple.JSON (class ReadForeign, class WriteForeign, readJSON', writeJSON)
+import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
+import Data.Variant (Variant)
+import Data.Variant as Variant
+import Foreign (F, Foreign, ForeignError(..), fail)
+import Foreign.Index (readProp)
+import Foreign.Object as Object
+import Global.Unsafe (unsafeStringify)
+import Partial.Unsafe (unsafeCrashWith)
+import Prim.Row as Row
+import Prim.RowList (class RowToList, kind RowList, Cons, Nil)
+import Simple.JSON (class ReadForeign, class WriteForeign, parseJSON, readImpl, readJSON', writeImpl, writeJSON)
+import Type.Data.RowList (RLProxy(..))
 import Type.Proxy (Proxy)
 import Unsafe.Coerce (unsafeCoerce)
 
@@ -66,8 +77,72 @@ data JSON a
 instance mediaTypeJSON :: MediaType (JSON a) where
   mediaType _ = Just applicationJSON
 
-instance encodeMediaJSON :: (WriteForeign a) => EncodeMedia (JSON a) a where
+instance encodeMediaJSONVariant ::
+  ( RowToList row rl
+  , WriteForeignVariant rl row
+  ) =>
+  EncodeMedia (JSON (Variant row)) (Variant row) where
+  encodeMedia _ = unsafeStringify <<< writeForeignVariant (RLProxy :: _ rl)
+else instance encodeMediaJSON ::
+  WriteForeign a =>
+  EncodeMedia (JSON a) a where
   encodeMedia _ = writeJSON
 
-instance decodeMediaJSON :: (ReadForeign a) => DecodeMedia (JSON a) a where
+instance decodeMediaJSONVariant ::
+  ( RowToList r rl
+  , ReadForeignVariant rl r
+  ) =>
+  DecodeMedia (JSON (Variant r)) (Variant r) where
+  decodeMedia _ = parseJSON >=> readForeignVariant (RLProxy :: _ rl)
+else instance decodeMediaJSON ::
+  ReadForeign a =>
+  DecodeMedia (JSON a) a where
   decodeMedia _ = readJSON'
+
+class ReadForeignVariant (rl :: RowList) (r :: #Type) | rl -> r where
+  readForeignVariant :: RLProxy rl -> Foreign -> F (Variant r)
+
+instance readForeignVariantNil :: ReadForeignVariant Nil r where
+  readForeignVariant _ _ = fail $ ForeignError "Unable to match any variant member."
+
+instance readForeignVariantCons ::
+  ( IsSymbol name
+  , ReadForeign value
+  , Row.Cons name value r' r
+  , ReadForeignVariant rl r
+  ) =>
+  ReadForeignVariant (Cons name value rl) r where
+  readForeignVariant _ o =
+    ( do
+        value :: value <- readImpl =<< readProp name o
+        pure $ Variant.inj proxy value
+    )
+      <|> readForeignVariant (RLProxy :: RLProxy rl) o
+    where
+    proxy = SProxy :: SProxy name
+
+    name = reflectSymbol proxy
+
+class WriteForeignVariant (rl :: RowList) (row :: #Type) | rl -> row where
+  writeForeignVariant :: forall proxy. proxy rl -> Variant row -> Foreign
+
+instance writeForeignVariantNil :: WriteForeignVariant Nil () where
+  writeForeignVariant _ _ = unsafeCrashWith "Variant was not able to be written. This should be unreachable"
+
+instance writeForeignVariantCons ::
+  ( IsSymbol name
+  , WriteForeign value
+  , Row.Cons name value r row
+  , WriteForeignVariant rl r
+  ) =>
+  WriteForeignVariant (Cons name value rl) row where
+  writeForeignVariant _ variant =
+    Variant.on
+      name
+      writeVariant
+      (writeForeignVariant (RLProxy :: _ rl))
+      variant
+    where
+    name = SProxy :: _ name
+
+    writeVariant value = writeImpl $ Object.insert (reflectSymbol name) (writeImpl value) Object.empty
