dafd14e2ab6c2f794ee3af4260cc6d132a7eef71
Robert Porter <robert.porter@rakuten.com>
2020-04-01 20:07:47 +0900

    Added in Response.toStatus for variant responses

--- src/Apiary/Client/Response.purs
+++ src/Apiary/Client/Response.purs
@@ -2,21 +2,23 @@ module Apiary.Client.Response where
 
 import Prelude
 import Apiary.Media (class DecodeMedia, decodeMedia)
-import Apiary.Status (class ResponseStatus)
+import Apiary.Status (class ResponseStatus, Status)
 import Apiary.Status as Status
 import Apiary.Types (Error(..), None, Response, Request, none)
 import Control.Alt ((<|>))
 import Control.Monad.Error.Class (throwError)
 import Control.Monad.Except (Except, withExcept)
+import Data.Maybe (isJust)
 import Data.Symbol (class IsSymbol)
-import Data.Variant (SProxy(..), Variant, expand, inj)
+import Data.Variant (SProxy(..), Variant, expand, inj, prj)
+import Partial.Unsafe (unsafeCrashWith)
 import Prim.Row (class Cons, class Union)
 import Prim.RowList (class RowToList, kind RowList, Cons, Nil)
 import Type.Data.RowList (RLProxy(..))
 import Type.Proxy (Proxy(..))
 
 class DecodeResponse rep response | rep -> response where
-  decodeResponse :: Proxy rep -> Response -> Except (Request -> Error) response
+  decodeResponse :: forall proxy. proxy rep -> Response -> Except (Request -> Error) response
 
 instance decodeResponseNone :: DecodeResponse None None where
   decodeResponse _ _ = pure none
@@ -31,8 +33,8 @@ instance decodeResponseRecord ::
   DecodeResponse { | responses } (Variant result) where
   decodeResponse _ = decodeResponseVariant (RLProxy :: _ responseList)
 
-class DecodeResponseVariant (response :: #Type) (responseList :: RowList) | responseList -> response where
-  decodeResponseVariant :: RLProxy responseList -> Response -> Except (Request -> Error) (Variant response)
+class DecodeResponseVariant (response :: # Type) (responseList :: RowList) | responseList -> response where
+  decodeResponseVariant :: forall proxy. proxy responseList -> Response -> Except (Request -> Error) (Variant response)
 
 instance decodeResponseVariantNil :: DecodeResponseVariant () Nil where
   -- | This will never be reached if the data originates from PureScript.
@@ -60,3 +62,31 @@ instance decodeResponseVariantCons ::
       | otherwise = throwError $ flip UnexpectedResponse response
 
     decodeRest = decodeResponseVariant (RLProxy :: _ responseList) response
+
+toStatus ::
+  forall response responseList.
+  RowToList response responseList =>
+  ResponseVariantToStatus response responseList =>
+  Variant response ->
+  Status
+toStatus = responseVariantToStatus (RLProxy :: _ responseList)
+
+class ResponseVariantToStatus (response :: # Type) (responseList :: RowList) | responseList -> response where
+  responseVariantToStatus :: forall proxy. proxy responseList -> Variant response -> Status
+
+instance responseVariantToStatusNil :: ResponseVariantToStatus response Nil where
+  -- | This will never be reached if the data originates from PureScript.
+  responseVariantToStatus _ _ = unsafeCrashWith "Unmatchable response variant"
+
+instance responseVariantToStatusCons ::
+  ( IsSymbol status
+  , ResponseStatus status
+  , Cons status body response' response
+  , ResponseVariantToStatus response responseList
+  ) =>
+  ResponseVariantToStatus response (Cons status body responseList) where
+  responseVariantToStatus _ response =
+    if isJust (prj (SProxy :: _ status) response) then
+      Status.toStatus (SProxy :: _ status)
+    else
+      responseVariantToStatus (RLProxy :: _ responseList) response
