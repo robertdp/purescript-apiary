b80586e43f081eb706cab9526dae80ae72b1c9aa
Robert Porter <robert.porter@rakuten.com>
2020-09-30 11:24:57 +0900

    Requests can be built safely now that Milkis is removed

--- src/Apiary/Client/Request.purs
+++ src/Apiary/Client/Request.purs
@@ -8,13 +8,13 @@ import Affjax.RequestHeader (RequestHeader(..))
 import Apiary.Client.Response (class DecodeResponse)
 import Apiary.Client.Url (class BuildUrl, buildUrl)
 import Apiary.Media (class EncodeMedia, class MediaType, encodeMedia, mediaType)
+import Apiary.Method (class RequestMethod, toMethod)
 import Apiary.Route (class PrepareSpec, Route)
 import Apiary.Types (None, Request)
 import Data.Array as Array
 import Data.Maybe (maybe)
 import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
 import Type.Proxy (Proxy(..))
-import Unsafe.Coerce (unsafeCoerce)
 
 class BuildRequest route params query body rep | route -> params query body rep where
   buildRequest :: route -> params -> query -> body -> Request
@@ -32,8 +32,8 @@ instance buildRequestRouteGET ::
   , IsSymbol path
   ) =>
   BuildRequest (Route "GET" path spec) params query None response where
-  buildRequest _ = buildRequest_ "GET" (SProxy :: _ path) (Proxy :: _ None)
-else instance buildRequestRoutePATCH ::
+  buildRequest _ = buildRequest_ (SProxy :: _ "GET") (SProxy :: _ path) (Proxy :: _ None)
+else instance buildRequestRouteRest ::
   ( PrepareSpec
       spec
       { path :: params
@@ -41,70 +41,24 @@ else instance buildRequestRoutePATCH ::
       , body :: body
       , response :: response
       }
+  , RequestMethod method
   , BuildUrl params query
   , MediaType body
   , EncodeMedia body body'
   , DecodeResponse response response'
   , IsSymbol path
   ) =>
-  BuildRequest (Route "PATCH" path spec) params query body' response where
-  buildRequest _ = buildRequest_ "PATCH" (SProxy :: _ path) (Proxy :: _ body)
-else instance buildRequestRoutePOST ::
-  ( PrepareSpec
-      spec
-      { path :: params
-      , query :: query
-      , body :: body
-      , response :: response
-      }
-  , BuildUrl params query
-  , MediaType body
-  , EncodeMedia body body'
-  , DecodeResponse response response'
-  , IsSymbol path
-  ) =>
-  BuildRequest (Route "POST" path spec) params query body' response where
-  buildRequest _ = buildRequest_ "POST" (SProxy :: _ path) (Proxy :: _ body)
-else instance buildRequestRoutePUT ::
-  ( PrepareSpec
-      spec
-      { path :: params
-      , query :: query
-      , body :: body
-      , response :: response
-      }
-  , BuildUrl params query
-  , MediaType body
-  , EncodeMedia body body'
-  , DecodeResponse response response'
-  , IsSymbol path
-  ) =>
-  BuildRequest (Route "PUT" path spec) params query body' response where
-  buildRequest _ = buildRequest_ "PUT" (SProxy :: _ path) (Proxy :: _ body)
-else instance buildRequestRouteDELETE ::
-  ( PrepareSpec
-      spec
-      { path :: params
-      , query :: query
-      , body :: body
-      , response :: response
-      }
-  , BuildUrl params query
-  , MediaType body
-  , EncodeMedia body body'
-  , DecodeResponse response response'
-  , IsSymbol path
-  ) =>
-  BuildRequest (Route "DELETE" path spec) params query body' response where
-  buildRequest _ = buildRequest_ "DELETE" (SProxy :: _ path) (Proxy :: _ body)
+  BuildRequest (Route method path spec) params query body' response where
+  buildRequest _ = buildRequest_ (SProxy :: _ method) (SProxy :: _ path) (Proxy :: _ body)
 
 buildRequest_ ::
-  forall path params query proxy bodyRep body.
+  forall method path params query proxy bodyRep body.
+  RequestMethod method =>
   IsSymbol path =>
   BuildUrl params query =>
   MediaType bodyRep =>
   EncodeMedia bodyRep body =>
-  String ->
+  SProxy method ->
   SProxy path ->
   proxy bodyRep ->
   params ->
@@ -112,8 +66,8 @@ buildRequest_ ::
   body ->
   Request
 buildRequest_ method path bodyRep params query body =
-  { method: unsafeCoerce method
-  , url: unsafeCoerce (buildUrl params query (reflectSymbol path))
+  { method: toMethod method
+  , url: buildUrl params query (reflectSymbol path)
   , headers: maybe [] (Array.singleton <<< ContentType) (mediaType bodyRep)
   , body: encodeMedia bodyRep body
   }
